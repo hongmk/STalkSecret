84016d7785dcb1269796e9f245d191a75736fb43
hongmk <isentator@naver.com>
2017-09-15 15:39:57 +0900

    15개 Empty Activiry 추가

---
+++ app/src/main/java/com/hongmk/stalksecret/CreateBoardActivity.java
@@ -0,0 +1,14 @@
+package com.hongmk.stalksecret;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_board);
+    }
+}
+
