71510b9b0954c74432cdeed6b6287bf47ddc9024
hongmk <isentator@naver.com>
2017-09-15 23:24:47 +0900

    레이아웃 수정/색상,스타일 xml활용/로그인화면전환

--- app/src/main/java/com/hongmk/stalksecret/MainActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/MainActivity.java
@@ -1,13 +1,34 @@
 package com.hongmk.stalksecret;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //2초뒤 핸들러 호출
+        handler.sendEmptyMessageDelayed(0, 1000*2);
+
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(msg.what == 0) {
+                //로그인 화면으로 전환
+                intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
