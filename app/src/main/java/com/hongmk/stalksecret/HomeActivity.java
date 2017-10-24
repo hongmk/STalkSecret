52c73155415c5acdf908dddf021b23a2502ee95f
hongmk <isentator@naver.com>
2017-10-24 21:56:17 +0900

    Home Tab 적용, 메뉴적용, 리스트아이템레이아웃수정

--- app/src/main/java/com/hongmk/stalksecret/HomeActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/HomeActivity.java
@@ -2,79 +2,92 @@ package com.hongmk.stalksecret;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
+import com.hongmk.stalksecret.fragment.HomeListFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentStatePagerItemAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
     Intent intent;
-    private WebView mWebView;
-    private String myUrl = "http://192.168.0.4:9000";
+    //아래 Stirng 값들이 가지는 것은 단순한 0,1,3...등의 인덱스가 아닌 고유한 객체의 값임
+    //다른데서 가져다쓸때는 MainActivity.tab10()[index] 로 가져올 수 있음
+    public static int[] tab10() {
+        return new int[] {
+                R.string.home_tab_1,
+                R.string.home_tab_2,
+                R.string.home_tab_3,
+                R.string.home_tab_4,
+                R.string.home_tab_5
+        };
+    }
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private SmartTabLayout viewPagerTab;
+    private FragmentPagerItems pages;
+    //FragmentPagerItemAdapter 사용 시 첫 생성 할 때 Tab1, Tab2에 모두 position 0으로 생성하므로 FragmentStatePagerItemAdapter 사용함.
+    private FragmentStatePagerItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
-        toolbar.setTitle("금융개발부"); //툴바 제목 표시여부
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        loadURL();
 
+        //toolbar.setTitle(R.string.demo_title_indicator_trick1); //툴바 제목 표시여부
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //왼쪽상단 BackKey 표시여부
 
-    }
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
 
-    public void onMyPage(View view){
-        intent = new Intent(HomeActivity.this, MypageActivity.class);
-        startActivity(intent);
-    }
+        pages = new FragmentPagerItems(this);
 
-    public void onNoti(View view){
-        intent = new Intent(HomeActivity.this, NoticeActivity.class);
-        startActivity(intent);
-    }
+        for (int titleResId : tab10()) {
+            //pages.add(FragmentPagerItem.of(getString(titleResId), DemoFragment.class));
+            pages.add(FragmentPagerItem.of(getString(titleResId), HomeListFragment.class));
+            Log.i("[SH]", "CREATE POSITION:"+ titleResId);
+        }
 
-    public void makeContent(View view) {
-        intent = new Intent(HomeActivity.this, CreateBoardActivity.class);
-        startActivity(intent);
-    }
+        adapter = new FragmentStatePagerItemAdapter(getSupportFragmentManager(), pages);
 
-
-
-    public void loadURL() {
-        mWebView = (WebView) findViewById(R.id.home_webView);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-
-        mWebView.loadUrl(myUrl); // 접속 URL
-        mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.setWebViewClient(new WebViewClientClass());
-
+        viewPager.setAdapter(adapter);
+        viewPagerTab.setViewPager(viewPager);
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    public class WebViewClientClass extends WebViewClient {
-        @SuppressWarnings("deprecation")
-        // 새로운 URL이 webview에 로드되려 할 경우 컨트롤을 대신할 기회를 줌
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.home_menu_mypage) {
+            intent = new Intent(HomeActivity.this, MypageActivity.class);
+            startActivity(intent);
+        } else if(item.getItemId() == R.id.home_menu_notice) {
+            intent = new Intent(HomeActivity.this, NoticeActivity.class);
+            startActivity(intent);
         }
+        return super.onOptionsItemSelected(item);
     }
 
-
+    public void createContentClick(View view){
+        intent = new Intent(HomeActivity.this, CreateBoardActivity.class);
+        startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/com/hongmk/stalksecret/fragment/HomeListFragment.java
@@ -0,0 +1,49 @@
+package com.hongmk.stalksecret.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hongmk.stalksecret.R;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+
+import java.util.ArrayList;
+
+public class HomeListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private HomeRecyclerAdapter adapter;
+    private ArrayList<HomeListItem> list = new ArrayList<HomeListItem>();
+    private static int previousSize =0;
+    int position;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home_list, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.home_recyclerView);
+
+        //Toast.makeText(getActivity(), "selected" + pos , Toast.LENGTH_SHORT).show();
+        /*현재 생성되는 Fragment의 인덱스를 가져옴
+        * 최초 생성 시 INDEX[0, 1]생성 -> 탭 1로 이동 시 INDEX[2]생성 하는 방식으로 동작함(선택된 다음 탭의 내용을 미리 생성함)
+        * */
+        int position = FragmentPagerItem.getPosition(getArguments());
+        Log.i("[SH]", "CREATE Fragment Position:"+ position);
+
+        list = HomeListItem.getList(10, position);
+
+        adapter = new HomeRecyclerAdapter(rootView.getContext(), list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext())); //세로로 스크롤 할 것이므로 LinearLayoutManager를 택함
+
+        return rootView;
+
+    }
+
+
+}
