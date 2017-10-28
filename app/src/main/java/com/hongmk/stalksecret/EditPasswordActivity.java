03da5f7d22033c6e793fd19b690ecf64a2a94210
hongmk <isentator@naver.com>
2017-10-28 16:22:12 +0900

    비밀번호 변경페이지 수정

--- app/src/main/java/com/hongmk/stalksecret/EditPasswordActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/EditPasswordActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class EditPasswordActivity extends AppCompatActivity {
 
@@ -26,5 +28,22 @@ public class EditPasswordActivity extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void verifyEditPassword(View view){
+        //비밀번호검증로직 (1. 변경 후 암호 2개가 일치하는지, 2.변경 전 암호가 맞는지) -> 둘 다 확인해서 한번에 결과리턴하기
+        Toast.makeText(EditPasswordActivity.this, "verifyEditPassword", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goMypage(View view){
+        onBackPressed();
+        finish();
+    }
+
+    public void completeEditPassword(View view){
+        //비밀번호 수정 통신
+        Toast.makeText(EditPasswordActivity.this, "암호변경 완료", Toast.LENGTH_SHORT).show();
+        onBackPressed();
+        finish();
     }
 }
