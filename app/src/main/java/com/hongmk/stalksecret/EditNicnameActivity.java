81ee7b289387656ea2fba387f8f1fc39e4ff6863
hongmk <isentator@naver.com>
2017-10-28 16:08:14 +0900

    닉네임변경페이지 수정

--- app/src/main/java/com/hongmk/stalksecret/EditNicnameActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/EditNicnameActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class EditNicnameActivity extends AppCompatActivity {
 
@@ -23,5 +25,22 @@ public class EditNicnameActivity extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void verifyEditNicname(View view){
+        //닉네임 검증 통신(signup페이지와 동일로직수행)
+        Toast.makeText(EditNicnameActivity.this, "verifyEditNicname", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goMypage(View view){
+        onBackPressed();
+        finish();
+    }
+
+    public void completeEditNicname(View view){
+        //닉네임 수정 통신
+        Toast.makeText(EditNicnameActivity.this, "닉네임변경 완료", Toast.LENGTH_SHORT).show();
+        onBackPressed();
+        finish();
     }
 }
