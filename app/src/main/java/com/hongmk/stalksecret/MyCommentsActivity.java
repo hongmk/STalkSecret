ff1e139b51ea8f933511217ebca41d57b63876ca
hongmk <isentator@naver.com>
2017-11-07 23:29:01 +0900

    내글, 내댓글 JSONArray로 생성 구현

--- app/src/main/java/com/hongmk/stalksecret/MyCommentsActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/MyCommentsActivity.java
@@ -1,6 +1,10 @@
 package com.hongmk.stalksecret;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
@@ -14,13 +18,28 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MyCommentsActivity extends AppCompatActivity {
 
     ListView listView = null;
+    ItemAdapter itemAdpater = null;
+    String user_nicname;
+    SharedPreferences user_nicname_pref;
+
+
     class Item {
         String cmTitle;
         String cmText;
@@ -52,7 +71,7 @@ public class MyCommentsActivity extends AppCompatActivity {
             super(conText, resource, objects);
         }
     }
-    ItemAdapter itemAdpater = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,14 +81,11 @@ public class MyCommentsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        listView = (ListView)findViewById(R.id.cmListView);
-        itemList.add(new Item("cmTitle01", "cmText0101"));
-        itemList.add(new Item("cmTitle02", "cmText0102"));
-        itemList.add(new Item("cmTitle03", "cmText0103"));
-        itemList.add(new Item("cmTitle04", "cmText0104"));
-        itemList.add(new Item("cmTitle05", "cmText0105"));
-        itemAdpater = new ItemAdapter(MyCommentsActivity.this, R.layout.my_comments_item, itemList);
-        listView.setAdapter(itemAdpater);
+        user_nicname_pref = getSharedPreferences("user_nicname", Activity.MODE_PRIVATE);
+        user_nicname = user_nicname_pref.getString("user_nicname", "");
+
+        //Toast.makeText(MyContentsActivity.this, ""+user_nicname, Toast.LENGTH_SHORT).show();
+        new GetComments().execute("http://192.168.0.4:52275/comments/commentlist?nicname="+""+user_nicname);
     }
 
     @Override
@@ -80,4 +96,93 @@ public class MyCommentsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    class GetComments extends AsyncTask<String, String, String> {
+        ProgressDialog dialog = new ProgressDialog(MyCommentsActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("GET");
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("글 조회중...");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            dialog.dismiss();
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                if (jsonArray.length() > 0) {
+
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        JSONObject json = jsonArray.getJSONObject(i);
+
+                        itemList.add(new Item(json.getString("content_id"), json.getString("comment")));
+                    }
+                    listView = (ListView)findViewById(R.id.cmListView);
+                    itemAdpater = new ItemAdapter(MyCommentsActivity.this, R.layout.my_comments_item, itemList);
+                    listView.setAdapter(itemAdpater);
+
+
+                } else {//인증실패
+
+                    Toast.makeText(MyCommentsActivity.this,
+                            "댓글 목록이 없습니다.",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+
+        }
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while(itr.hasNext()){
+
+                String key= itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
+    }
 }
--- app/src/main/java/com/hongmk/stalksecret/MyContentsActivity.java
+++ app/src/main/java/com/hongmk/stalksecret/MyContentsActivity.java
@@ -1,6 +1,10 @@
 package com.hongmk.stalksecret;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
@@ -14,13 +18,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MyContentsActivity extends AppCompatActivity {
 
     ListView listView = null;
+    String user_nicname;
+    SharedPreferences user_nicname_pref;
+    ItemAdapter itemAdpater = null;
+
     class Item {
         String title;
         String text;
@@ -52,7 +70,7 @@ public class MyContentsActivity extends AppCompatActivity {
             super(context, resource, objects);
         }
     }
-    ItemAdapter itemAdpater = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,23 +81,111 @@ public class MyContentsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        user_nicname_pref = getSharedPreferences("user_nicname", Activity.MODE_PRIVATE);
+        user_nicname = user_nicname_pref.getString("user_nicname", "");
 
-        listView = (ListView)findViewById(R.id.listView);
-        itemList.add(new Item("Title01", "Text0101"));
-        itemList.add(new Item("Title02", "Text0102"));
-        itemList.add(new Item("Title03", "Text0103"));
-        itemList.add(new Item("Title04", "Text0104"));
-        itemList.add(new Item("Title05", "Text0105"));
-        itemAdpater = new ItemAdapter(MyContentsActivity.this, R.layout.my_contents_item,
-                itemList);
-        listView.setAdapter(itemAdpater);
+        //Toast.makeText(MyContentsActivity.this, ""+user_nicname, Toast.LENGTH_SHORT).show();
+        new GetContents().execute("http://192.168.0.4:52275/contents/contentlist?nicname="+""+user_nicname);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             onBackPressed();
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
+
+    class GetContents extends AsyncTask<String, String, String> {
+        ProgressDialog dialog = new ProgressDialog(MyContentsActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("GET");
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("글 조회중...");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            dialog.dismiss();
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                if (jsonArray.length() > 0) {//인증 성공
+
+                    for (int i = 0; i < jsonArray.length(); i++){
+                        JSONObject json = jsonArray.getJSONObject(i);
+
+                        itemList.add(new Item(json.getString("title"), json.getString("content")));
+                    }
+                    listView = (ListView)findViewById(R.id.listView);
+                    itemAdpater = new ItemAdapter(MyContentsActivity.this, R.layout.my_contents_item,  itemList);
+                    listView.setAdapter(itemAdpater);
+
+                } else {//인증실패
+
+                    Toast.makeText(MyContentsActivity.this,
+                            "글목록이 없습니다.",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+
+        }
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while(itr.hasNext()){
+
+                String key= itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
+    }
+
+
 }
